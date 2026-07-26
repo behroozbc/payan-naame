790bf13d7fa24bb34195b434decf0ec098c7bdae
behroozbc <behroozbc@outlook.com>
2026-07-26 22:40:26 +0000

    Deploying to gh-pages from  @ a8dcd84ce704e661db1a794ef453cdcd6d56b088 🚀

---
+++ assets/md-CBz_BmUl.js
@@ -0,0 +1,12 @@
+import{_ as u}from"./slidev/VClick-DR5qr6H6.js";import{_ as m}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-DVv6UW_P.js";import{o as d,b as c,w as t,g as s,d as a,m as f,D as e,v as _,x as g,z as l}from"./modules/vue-vrn8koTT.js";import{I as k}from"./slidev/default-cOFAplKV.js";import{u as h,f as v}from"./slidev/context-ovyuteKM.js";import"./index-BNn9Goy2.js";import"./modules/shiki-Bmk7LGeR.js";import"./slidev/VClicks-Bv7aaqYB.js";import"./modules/unplugin-icons-BBqRiiFx.js";const V={__name:"raft-pipeline.md__slidev_22",setup(x){const{$clicksContext:r,$frontmatter:i}=h();return r.setup(),(b,n)=>{const o=m,p=u;return d(),c(k,_(g(l(v)(l(i),21))),{default:t(()=>[n[1]||(n[1]=s("h3",null,"Building the Prompt",-1)),n[2]||(n[2]=s("p",null,"The prompt presents data in a structured manner so the model understands relationships between components:",-1)),a(p,null,{default:t(()=>[a(o,f({},{title:"",ranges:[]}),{default:t(()=>[...n[0]||(n[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"You are a question-answering model for students.")]),e(`
+`),s("span",{class:"line"},[s("span",null,"Use all material provided in the context.")]),e(`
+`),s("span",{class:"line"},[s("span",null,"If the user does not know some prerequisites, cover them first.")]),e(`
+`),s("span",{class:"line"},[s("span",null,"Answer in an academic tone.")]),e(`
+`),s("span",{class:"line"},[s("span")]),e(`
+`),s("span",{class:"line"},[s("span",null,"User query: {user query}")]),e(`
+`),s("span",{class:"line"},[s("span")]),e(`
+`),s("span",{class:"line"},[s("span",null,"Retrieved documents: {retrieved documents}")]),e(`
+`),s("span",{class:"line"},[s("span")]),e(`
+`),s("span",{class:"line"},[s("span",null,"Symbol understanding levels: {symbol_level}")]),e(`
+`),s("span",{class:"line"},[s("span")]),e(`
+`),s("span",{class:"line"},[s("span",null,"Prerequisites status & content: {prerequisites_level_documents}")])])],-1)])]),_:1},16)]),_:1})]),_:1},16)}}};export{V as default};
